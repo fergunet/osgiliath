0c1645f0877e67d06cc2ee703ae7dbac357b6115
fergunet <fergunet@gmail.com>
2011-09-27 08:21:24 +0000

    Modified the Parameter reader from a file

--- OsgiliathEvolutionaryAlgorithm/src/es/ugr/osgiliath/evolutionary/elements/EvolutionaryBasicParameters.java
+++ OsgiliathEvolutionaryAlgorithm/src/es/ugr/osgiliath/evolutionary/elements/EvolutionaryBasicParameters.java
@@ -6,6 +6,14 @@ import es.ugr.osgiliath.algorithms.AlgorithmParameters;
 
 public interface EvolutionaryBasicParameters extends AlgorithmParameters{
 	
+	static final String NUM_GENERATIONS_PROP = "parameters.evolutionary.generations";
+	static final String CROSSOVER_PROB_PROP = "parameters.evolutionary.crossoverprob";
+	static final String MUTATION_PROB_PROP = "parameters.evolutionary.mutationprob";
+	static final String POPULATION_SIZE_PROP = "parameters.evolutionary.populationsize";
+	static final String PARENT_SELECTOR_SIZE_PROP = "parameters.evolutionary.selectorsize";
+	static final String REPLACER_SIZE_PROP = "parameters.evolutionary.replacersize";
+
+	
 	int getNumGenerations();
 	
 	double getCrossoverProb();

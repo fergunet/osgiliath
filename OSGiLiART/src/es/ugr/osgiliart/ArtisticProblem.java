26dde336ee0ad16f4ea3864df7766e8b145d6f01
fergunet <fergunet@gmail.com>
2013-03-09 18:00:24 +0000

    Añadido ArtisticProblem

---
+++ OSGiLiART/src/es/ugr/osgiliart/ArtisticProblem.java
@@ -0,0 +1,39 @@
+package es.ugr.osgiliart;
+
+import es.ugr.osgiliath.problem.InputData;
+import es.ugr.osgiliath.problem.Problem;
+import es.ugr.osgiliath.problem.ProblemParameters;
+
+public class ArtisticProblem implements Problem{
+
+	@Override
+	public void setInputData(InputData data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setProblemParameters(ProblemParameters problemParams) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unsetProblemParameters(ProblemParameters problemParams) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ProblemParameters getParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InputData getInputData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

9515304c6f101bd8eaf61d6cc128f2a72510b8e9
fergunet <fergunet@gmail.com>
2013-03-09 19:00:06 +0000

    Artistic Initializer modificado

--- OSGiLiART/src/es/ugr/osgiliart/ArtisticInitializer.java
+++ OSGiLiART/src/es/ugr/osgiliart/ArtisticInitializer.java
@@ -2,15 +2,28 @@ package es.ugr.osgiliart;
 
 import java.util.ArrayList;
 
+import es.ugr.osgiliath.OsgiliathService;
+import es.ugr.osgiliath.evolutionary.elements.FitnessCalculator;
 import es.ugr.osgiliath.evolutionary.individual.Individual;
 import es.ugr.osgiliath.evolutionary.individual.Initializer;
 
-public class ArtisticInitializer implements Initializer {
+public class ArtisticInitializer extends OsgiliathService implements Initializer {
 
+	private FitnessCalculator fitnessCalculator;
+	
 	@Override
 	public ArrayList<Individual> initializeIndividuals(int size) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void setFitnessCalculator(FitnessCalculator fitnessCalculator){
+		this.fitnessCalculator = fitnessCalculator;
+	}
+	
+	public void unsetFitnessCalculator(FitnessCalculator fitnessCalculator){
+		this.fitnessCalculator = null;
+	}
+	
 
 }

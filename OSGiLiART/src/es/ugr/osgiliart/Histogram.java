c66d0ca5064497b538a2438eae50aacca10b83b3
abpelegrina <anabpel@gmail.com>
2013-03-10 15:33:15 +0000

    refactoriza

--- OSGiLiART/src/es/ugr/osgiliart/Histogram.java
+++ OSGiLiART/src/es/ugr/osgiliart/Histogram.java
@@ -5,50 +5,59 @@ import processing.core.*;
 @SuppressWarnings("serial")
 public class Histogram extends PApplet{
 	
-	public static void Test(){
+	public static final int HUE = 1;
+	public static final int SATURATION = 2;
+	public static final int BRIGHTNESS = 3;
+	public static final int RED = 4;
+	public static final int GREEN = 5;
+	public static final int BLUE = 6;
+	
+	public static void Test(String filename){
 		Histogram h = new Histogram();
 		h.init();
 		h.setup();
 		
-		System.out.println("HSV:");
 		
-		// HUE
+		
+		System.out.println("HSV:");
+		double[] histogram;
+		/*/ HUE
 		System.out.print("H: ");
-		int[] histogram = h.getHueHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		double[] histogram = h.getHueHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");
 		
 		//SATURATION
 		System.out.print("\nS: ");
-		histogram = h.getSaturationHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		histogram = h.getSaturationHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");
 		
 		//VALUE
 		System.out.print("\nV: ");
-		histogram = h.getBrightnessHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		histogram = h.getBrightnessHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");
 		
 		System.out.println("\nRGB: ");
 		
 		// HUE
 		System.out.print("R: ");
-		histogram = h.getRedHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		histogram = h.getRedHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");
 		
 		//SATURATION
 		System.out.print("\nG: ");
-		histogram = h.getGreenHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		histogram = h.getGreenHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");*/
 		
 		//VALUE
 		System.out.print("\nB: ");
-		histogram = h.getBlueHistogram("/Users/anabpel/Documents/workspace/osgilath/osgiliath/OSGiLiART/scripts/collage-2.png");
+		histogram = h.getBlueHistogram(filename);
 		for(int i=0; i<histogram.length; ++i)
-			System.out.print(" "+histogram[i]);
+			System.out.print(" "+histogram[i]+",");
 		
 	}
 	
@@ -57,12 +66,8 @@ public class Histogram extends PApplet{
 		    background(0);
 	}
 	
-	/**
-	 * Get the BRIGHTNESS histogram for an image
-	 * @param filename name of the file
-	 * @return 
-	 */
-	public int[]  getHueHistogram(String filename){
+	
+	protected double[]  getHistogram(String filename, int type){
 		// The next line is needed if running in JavaScript Mode with Processing.js
 		/* @pjs preload="frontier.jpg"; */ 
 
@@ -72,39 +77,74 @@ public class Histogram extends PApplet{
 		image(img, 0, 0);
 		int[] hist = new int[256];
 
+		int total = 0;
 		// Calculate the histogram
 		for (int i = 0; i < img.width; i++) {
 		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) hue(get(i, j));
-		    hist[bright]++; 
+			int value;
+			switch(type){
+			case HUE:
+				value = (int) hue(get(i, j));
+				break;
+			case SATURATION:
+				value = (int) saturation(get(i, j));
+				break;
+			case BRIGHTNESS:
+				value = (int) brightness(get(i, j));
+				break;
+			case RED:
+				value = (int) red(get(i, j));
+				break;
+			case GREEN:
+				value = (int) green(get(i, j));
+				break;
+			case BLUE:
+				value = (int) blue(get(i, j));
+				break;
+			default:
+				value = (int) hue(get(i, j));
+				break;
+			}
+			if (value > 0){
+				 hist[value]++;
+			}
 		  }
 		}
-		return hist;
+		
+		// Find the largest value in the histogram
+		double histMax = max(hist);
+		
+		/*if (total == histMax)
+			System.out.println("coinciden");*/
+		
+		//System.out.println("MAX: " + histMax);
+		
+		double[] histD = new double[256];
+		
+		for (int i=0; i<hist.length; ++i)
+			histD[i] = (double)hist[i] / ((double)img.height*img.width) ;
+		
+		return histD;
 	}
+	
+	
+	/**
+	 * Get the HUE histogram for an image
+	 * @param filename name of the file
+	 * @return 
+	 */
+	public double[]  getHueHistogram(String filename){
+		return getHistogram(filename, HUE);
+	}
+	
 	
 	/**
 	 * Get the SATURATION histogram for an image
 	 * @param filename name of the file
 	 * @return 
 	 */
-	public int[]  getSaturationHistogram(String filename){
-		// The next line is needed if running in JavaScript Mode with Processing.js
-		/* @pjs preload="frontier.jpg"; */ 
-
-		// Load an image from the data directory
-		// Load a different image by modifying the comments
-		PImage img = loadImage(filename);
-		image(img, 0, 0);
-		int[] hist = new int[256];
-
-		// Calculate the histogram
-		for (int i = 0; i < img.width; i++) {
-		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) saturation(get(i, j));
-		    hist[bright]++; 
-		  }
-		}
-		return hist;
+	public double[]  getSaturationHistogram(String filename){
+		return getHistogram(filename, SATURATION);
 	}
 	
 	/**
@@ -112,24 +152,8 @@ public class Histogram extends PApplet{
 	 * @param filename name of the file
 	 * @return 
 	 */
-	public int[]  getBrightnessHistogram(String filename){
-		// The next line is needed if running in JavaScript Mode with Processing.js
-		/* @pjs preload="frontier.jpg"; */ 
-
-		// Load an image from the data directory
-		// Load a different image by modifying the comments
-		PImage img = loadImage(filename);
-		image(img, 0, 0);
-		int[] hist = new int[256];
-
-		// Calculate the histogram
-		for (int i = 0; i < img.width; i++) {
-		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) brightness(get(i, j));
-		    hist[bright]++; 
-		  }
-		}
-		return hist;
+	public double[]  getBrightnessHistogram(String filename){
+		return getHistogram(filename, BRIGHTNESS);
 	}
 	
 	/**
@@ -137,25 +161,8 @@ public class Histogram extends PApplet{
 	 * @param filename name of the file
 	 * @return 
 	 */
-	public int[]  getRedHistogram(String filename){
-		// The next line is needed if running in JavaScript Mode with Processing.js
-		/* @pjs preload="frontier.jpg"; */ 
-
-		// Load an image from the data directory
-		// Load a different image by modifying the comments
-		PImage img = loadImage(filename);
-		image(img, 0, 0);
-		int[] hist = new int[256];
-
-		// Calculate the histogram
-		for (int i = 0; i < img.width; i++) {
-		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) red(get(i, j));
-		    hist[bright]++; 
-		  }
-		}
-
-		return hist;
+	public double[]  getRedHistogram(String filename){
+		return getHistogram(filename, RED);
 	}
 	
 	/**
@@ -163,25 +170,8 @@ public class Histogram extends PApplet{
 	 * @param filename name of the file
 	 * @return 
 	 */
-	public int[]  getBlueHistogram(String filename){
-		// The next line is needed if running in JavaScript Mode with Processing.js
-		/* @pjs preload="frontier.jpg"; */ 
-
-		// Load an image from the data directory
-		// Load a different image by modifying the comments
-		PImage img = loadImage(filename);
-		image(img, 0, 0);
-		int[] hist = new int[256];
-
-		// Calculate the histogram
-		for (int i = 0; i < img.width; i++) {
-		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) blue(get(i, j));
-		    hist[bright]++; 
-		  }
-		}
-
-		return hist;
+	public double[]  getBlueHistogram(String filename){
+		return getHistogram(filename, BLUE);
 	}
 	
 	/**
@@ -189,24 +179,7 @@ public class Histogram extends PApplet{
 	 * @param filename name of the file
 	 * @return 
 	 */
-	public int[]  getGreenHistogram(String filename){
-		// The next line is needed if running in JavaScript Mode with Processing.js
-		/* @pjs preload="frontier.jpg"; */ 
-
-		// Load an image from the data directory
-		// Load a different image by modifying the comments
-		PImage img = loadImage(filename);
-		image(img, 0, 0);
-		int[] hist = new int[256];
-
-		// Calculate the histogram
-		for (int i = 0; i < img.width; i++) {
-		  for (int j = 0; j < img.height; j++) {
-		    int bright = (int) green(get(i, j));
-		    hist[bright]++; 
-		  }
-		}
-
-		return hist;
+	public double[]  getGreenHistogram(String filename){
+		return getHistogram(filename, GREEN);
 	}
 }

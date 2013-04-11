a7ee12b7797be8f2b27fdf804f2e7ed5d064add2
fergunet <fergunet@gmail.com>
2013-04-11 16:16:40 +0000

    Eliminado el fitness extra en el DoubleFitness de ArtisticHistogramCalculator (para estadisticas)

--- OSGiLiART/src/es/ugr/osgiliart/fitnesscalculators/ArtisticHistogramFitnessCalculator.java
+++ OSGiLiART/src/es/ugr/osgiliart/fitnesscalculators/ArtisticHistogramFitnessCalculator.java
@@ -1,9 +1,31 @@
+/*
+ * ArtisticHistogramFitnessCalculator.java
+ * 
+ * Copyright (c) 2013, Pablo Garcia-Sanchez. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * Contributors:
+ */
 package es.ugr.osgiliart.fitnesscalculators;
 
-import java.io.FileInputStream;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Properties;
+
 
 import es.ugr.osgiliart.ArtisticIndividual;
 import es.ugr.osgiliart.HistogramExtractor;
@@ -97,7 +119,7 @@ public class ArtisticHistogramFitnessCalculator extends OsgiliathService impleme
 		//df.theOther = 1-fitnessHSV;
 		
 		DoubleFitness df = new DoubleFitness(1- fitnessHSV,true);
-		df.theOther = 1-fitnessRGB;
+		//df.theOther = 1-fitnessRGB;
 		
 		//double average = (1-fitnessHSV + 1-fitnessRGB)/2.0;
 		//DoubleFitness df = new DoubleFitness(average,true);

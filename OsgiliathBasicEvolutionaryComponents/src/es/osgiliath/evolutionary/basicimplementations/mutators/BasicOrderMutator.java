f758a7241caaf0b4e41e2a11b4aa1d6b00eecee7
fergunet <fergunet@gmail.com>
2013-04-11 11:35:38 +0000

    Añadidas cabeceras LGPL.

--- OsgiliathBasicEvolutionaryComponents/src/es/osgiliath/evolutionary/basicimplementations/mutators/BasicOrderMutator.java
+++ OsgiliathBasicEvolutionaryComponents/src/es/osgiliath/evolutionary/basicimplementations/mutators/BasicOrderMutator.java
@@ -1,3 +1,25 @@
+/*
+ * BasicOrderMutator.java
+ * 
+ * Copyright (c) 2013, Pablo Garcia-Sanchez. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * Contributors:
+ */
 package es.osgiliath.evolutionary.basicimplementations.mutators;
 
 import java.util.ArrayList;
@@ -25,8 +47,8 @@ public class BasicOrderMutator extends OsgiliathService implements Mutator{
 			
 			for(Individual ind: individuals){
 				
-				double prob = (Double) this.getAlgorithmParameters().getParameter(EvolutionaryBasicParameters.MUTATOR_PROB);
-				double rand = Math.random();
+				//double prob = (Double) this.getAlgorithmParameters().getParameter(EvolutionaryBasicParameters.MUTATOR_PROB);
+				//double rand = Math.random();
 				//double stepSize = (Double) this.getAlgorithmParameters().getParameter(EvolutionaryBasicParameters.MUTATOR_STEPSIZE);
 				//TODO ACUStepSize?
 				

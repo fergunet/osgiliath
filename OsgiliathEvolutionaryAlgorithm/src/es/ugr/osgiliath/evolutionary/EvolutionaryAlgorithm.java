7c03b24498c14afdef44941b2b951ce92775d4ba
fergunet <fergunet@gmail.com>
2013-04-11 14:30:39 +0000

    añadidas cabeceras LGPL y eliminado DistributedIslandGeneticAlgorithm

--- OsgiliathEvolutionaryAlgorithm/src/es/ugr/osgiliath/evolutionary/EvolutionaryAlgorithm.java
+++ OsgiliathEvolutionaryAlgorithm/src/es/ugr/osgiliath/evolutionary/EvolutionaryAlgorithm.java
@@ -1,3 +1,25 @@
+/*
+ * EvolutionaryAlgorithm.java
+ * 
+ * Copyright (c) 2013, Pablo Garcia-Sanchez. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * Contributors:
+ */
 package es.ugr.osgiliath.evolutionary;
 
 import java.util.ArrayList;
@@ -47,7 +69,7 @@ public class EvolutionaryAlgorithm extends OsgiliathService implements Algorithm
 	}
 	
 	public void activate(){
-		System.out.println("ALGORITMO ACTIVADO");
+		System.out.println("EVOLUTIONARY ALGORITHM ACTIVATED");
 		//parameters.setup(null);
 		
 		//this.start();
@@ -89,8 +111,8 @@ public class EvolutionaryAlgorithm extends OsgiliathService implements Algorithm
 			//this.getEventAdmin();
 			actualIteration++;
 			//System.out.println("IT "+actualIteration);
-			System.out.println(this.getObtainedSolution().getSolutionValue());
-			logger.statsX(timeSelection+";"+timeRecombination+";"+timeMutation+";"+timeReplacement+"\n", "times");
+			//System.out.println(this.getObtainedSolution().getSolutionValue());
+			//logger.statsX(timeSelection+";"+timeRecombination+";"+timeMutation+";"+timeReplacement+"\n", "times");
 		}while(!stopCriterion.hasFinished());
 		
 		

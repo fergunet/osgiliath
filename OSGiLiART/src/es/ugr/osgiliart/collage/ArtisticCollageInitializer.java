220a594fe8acc0657968e8045dfce217d640fefd
fergunet <fergunet@gmail.com>
2013-04-23 10:31:40 +0000

    Añadidas cabeceras LGPL, eliminados archivos no usados y cambios de paquete de funciones Collage

--- OSGiLiART/src/es/ugr/osgiliart/collage/ArtisticCollageInitializer.java
+++ OSGiLiART/src/es/ugr/osgiliart/collage/ArtisticCollageInitializer.java
@@ -1,7 +1,32 @@
-package es.ugr.osgiliart;
+/*
+ * ArtisticCollageInitializer.java
+ * 
+ * Copyright (c) 2013, Pablo Garcia-Sanchez. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * Contributors:
+ */
+package es.ugr.osgiliart.collage;
 
 import java.util.ArrayList;
 
+import es.ugr.osgiliart.ArtisticGenome;
+import es.ugr.osgiliart.ArtisticIndividual;
+import es.ugr.osgiliart.ArtisticParameters;
 import es.ugr.osgiliart.core.generators.PathGenerator;
 import es.ugr.osgiliart.core.generators.point.RandomPointGenerator;
 import es.ugr.osgiliart.core.rand.RandU;

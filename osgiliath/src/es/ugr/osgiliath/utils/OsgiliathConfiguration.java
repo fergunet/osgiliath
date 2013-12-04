c4b0072219849921dc7a9eba9937ee3a22e96cb2
fergunet <fergunet@gmail.com>
2013-12-04 15:04:49 +0000

    Añadido parametro numthreads

--- osgiliath/src/es/ugr/osgiliath/utils/OsgiliathConfiguration.java
+++ osgiliath/src/es/ugr/osgiliath/utils/OsgiliathConfiguration.java
@@ -7,6 +7,7 @@ public interface OsgiliathConfiguration {
 	public static final String PORT_PROP = "osgiliathPort";
 	public static final String PARAMETER_FILE_PROP = "parameterFile";
 	public static final String LOG_FOLDER = "configuration.log.folder";
+	public static final String NUM_THREADS = "configuration.numthreads";
 	public static final String PROBLEM_NAME = "problem.name";
 	
 	public void configure();

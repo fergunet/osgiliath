5d00a0b7cbc55e853dd5c7c6f0a73d5b039887e2
Antonio Fernández Ares <antares.es@gmail.com>
2014-10-17 16:45:52 +0200

    Stuff 3

---
+++ PlanetWars/src/es/ugr/osgiliath/planetwars/fitness/PlanetWarsHierarchicalFitnessMaximization.java
@@ -0,0 +1,42 @@
+package es.ugr.osgiliath.planetwars.fitness;
+
+public class PlanetWarsHierarchicalFitnessMaximization extends PlanetWarsHierarchicalFitness {
+
+	public PlanetWarsHierarchicalFitnessMaximization(double wins, double turns) {
+		super(wins, turns);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int compareTo(Object o) {
+		
+		PlanetWarsHierarchicalFitness other = (PlanetWarsHierarchicalFitness)o;
+		
+		//negative if a<b
+		
+		if(this.primaryFitness < other.primaryFitness)
+			return -1;
+		if(this.primaryFitness > other.primaryFitness)
+			return 1;
+		if(this.primaryFitness == other.primaryFitness)
+			if(this.secondaryFitness < other.secondaryFitness)
+				return -1;
+			else
+				return 1;
+		return 0;
+	}
+	
+	@Override
+	public void setAsWorstValue() {
+		this.secondaryFitness = Integer.MAX_VALUE;
+		this.primaryFitness = Integer.MAX_VALUE;
+		
+	}
+
+	@Override
+	public void setAsBestValue() {
+		this.primaryFitness = Integer.MIN_VALUE;
+		this.secondaryFitness = 0;
+	}
+
+}
